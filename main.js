c817f637377b98bc98b5255b3f966d5e3bf49304
Rémi Lamotte <rl01.octave@octave.local>
2017-04-04 16:26:25 +0200

    Update

--- main.js
+++ main.js
@@ -12,21 +12,6 @@ function connect() {
             console.log('Connecting to GATT Server...');
             return device.gatt.connect();
         })
-        .then(server => {
-          // Getting Battery Service...
-          return server.getPrimaryService('battery_service');
-        })
-        .then(service => {
-          // Getting Battery Level Characteristic...
-          return service.getCharacteristic('battery_level');
-        })
-        .then(characteristic => {
-          // Reading Battery Level...
-          return characteristic.readValue();
-        })
-        .then(value => {
-          console.log('Battery percentage is ' + value.getUint8(0));
-        })
         .catch(error => {
             console.log('Argh! ' + error);
         });
